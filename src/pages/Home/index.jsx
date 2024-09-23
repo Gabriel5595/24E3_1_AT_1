c724e8bac41c0d8b37d25e103645b82f807b147a
Gabriel5595 <gabriel.vcarvalho@al.infnet.edu.br>
2024-09-23 19:51:34 -0300

    Finalização do Projeto

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,8 +2,19 @@ import styles from "./home.module.css"
 
 export default function Home() {
     return (
-        <div>
-            <h1>Template</h1>
-        </div>
+        <header className={styles.menu}>
+            <div className={styles.logo}>
+                <img src="https://www.adaptivewfs.com/wp-content/uploads/2020/07/logo-placeholder-image.png" alt="Brand Logo" />
+                <span>Brand</span>
+            </div>
+            <nav className={styles.menuItems}>
+                <a href="#item1">Produtos</a>
+                <a href="#item2">Serviços</a>
+                <a href="#item3">Contato</a>
+            </nav>
+            <div className={styles.userIcon}>
+                <img src="https://cdn-icons-png.flaticon.com/512/1144/1144760.png" alt="User Icon" />
+            </div>
+        </header>
     )
 }
